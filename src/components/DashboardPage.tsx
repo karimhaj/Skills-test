592f9cc39f04aa619af42d4b74bd1b70b3db7b92
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-11-13 11:17:46 +0100

    create data folder and contactlist to fill dashboard cards

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,25 +1,38 @@
 import React from 'react'; 
+import PropTypes from 'prop-types';
 import { CssBaseline, Grid, createTheme, ThemeProvider, Box, Typography } from '@mui/material';
+
+import peopleList from '../data/contactList'; 
+
+interface personObjTypes{
+  name?: string, 
+  id?: number,
+  age?: number,
+  number?: string,
+  picture?: string,
+  children?: JSX.Element|JSX.Element[];
+}
 
 //inline styles
 
-const titleSectionStyle={
-  border: '1px solid gray',
-  padding: '10px',
-  borderRadius: '10px'
-}
+// const titleSectionStyle={
+//   border: '1px solid gray',
+//   padding: '10px',
+//   borderRadius: '10px'
+// }
 
-const cardStyle={
-  border: '1px solid gray',
-  borderRadius: '10px', 
-  height: '200px'
-}
+// const cardStyle={
+//   border: '1px solid gray',
+//   borderRadius: '10px', 
+//   height: '200px'
+// }
 
-const dyagramStyle={
-  border: '1px solid gray',
-  borderRadius: '10px', 
-  height: '500px'
-}
+// const dyagramStyle={
+//   border: '1px solid gray',
+//   borderRadius: '10px', 
+//   height: '500px'
+// }
+
 
 const themeOptions = {
     palette: {
@@ -45,7 +58,23 @@ return (<div>
             alignItems: 'center',
             margin: '50px'
     }}>
+    
+    <h1 style={{marginBottom: '0px'}}>Dashboard Page</h1>
+    <p style={{marginTop: '0px'}}>subtitle for the dash</p>
 
+    <div style={{display: 'flex'}}>
+    {
+      peopleList.map((item)=>
+        <Card 
+        key={`${item.id}`}
+        name={item.name}
+        picture={item.picture}
+        age={item.age}
+        number={item.number}
+        />)
+    }
+        </div>
+{/* 
     <Grid container 
     direction='row'
     justifyContent='center'
@@ -61,7 +90,7 @@ return (<div>
         </Typography>
     </div>
   </Grid>
-  {/* <Grid item xs={12} sm={6} md={8}>
+  <Grid item xs={12} sm={6} md={8}>
     <div style={titleSectionStyle}>xs=6 md=4</div>
   </Grid>
   <Grid item xs={12} sm={12} md={8}>
@@ -81,12 +110,36 @@ return (<div>
 
   <Grid item xs={12} sm={6} md={4}>
   <div style={cardStyle}>card1</div>
-  </Grid> */}
+  </Grid>
 
-    </Grid>
+    </Grid> */}
     </Box>
     </ThemeProvider>
 </div>)
 }
 
+
+
+function Card (props: personObjTypes){
+
+
+  return(
+    <div style={{
+      border: '1px solid rgb(200,200,200)',
+      padding: '10px',
+      margin: '10px',
+      borderRadius: '10px'}}>
+      <div style={{display:'flex', justifyContent: 'space-between'}}>
+      <h2>{props.name}</h2>
+
+      <div style={{width:'50px', height:'50px', borderRadius:'50px'}}>
+      <img src={props.picture} alt='propic' style={{width: '50px', borderRadius: '50px', objectFit: 'contain'}}/>
+      </div>
+
+      </div>
+      <h3>Age: {props.age}</h3>
+      <p>Number: {props.number} </p>
+    </div>
+  )
+}
 export default Dashboard; 
---
+++ src/data/contactList.js
@@ -0,0 +1,33 @@
+const peopleList = [
+    {
+      name: 'Luke',
+      id: 1,
+      age: 30,
+      number: '348 098 1112',
+      picture: 'https://images.squarespace-cdn.com/content/v1/53b599ebe4b08a2784696956/1495768199675-YC0K3ZBS92FSN1SD49HR/professional-photographer-05.jpg' 
+    },
+    {
+      name: 'Sarah',
+      id: 2,
+      age: 34,
+      number: '351 953 1067',
+      picture: 'https://images.squarespace-cdn.com/content/v1/53b599ebe4b08a2784696956/1504755539389-NUA1FAUO2KA2VU4GLK43/professional-headshots-nyc-051.jpg?format=1000w'
+    },
+    {
+      name: 'Jeremy',
+      id: 3,
+      age: 36,
+      number: '390 400 6363',
+      picture: 'https://hairstyleonpoint.com/wp-content/uploads/2020/11/Business-Professional-Mens-Hairstyles.jpg'
+    },
+    {
+      name: 'Veronica',
+      id: 4,
+      age: 28,
+      number: '370 965 5006',
+      picture: 'https://i.pinimg.com/originals/77/71/68/7771683223d86b237a3304d6f32828b9.jpg'
+    }
+  
+  ]; 
+
+  export default peopleList;

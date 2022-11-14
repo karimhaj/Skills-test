b0c11088b7b78d4b5883ee19c2aacbfbc9510160
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-11-14 11:17:35 +0100

    add and start customizing AppBar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 import {Routes, Route, Link} from 'react-router-dom';
 import LoginPage from './components/LoginPage'; 
 import DashboardPage from './components/DashboardPage';
+import AppBar from './components/AppBar'; 
 
 function App() {
   return (<>
+  <AppBar />
     <Routes>
       <Route path='/' element={ <LoginPage />} />
       <Route path='/dashboard' element={ <DashboardPage />} />
